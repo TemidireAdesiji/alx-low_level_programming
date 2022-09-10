9a261e256f2efe9f6f7f11bdce205b1f998e7255
TemidireAdesiji <temidire2002@gmail.com>
2022-09-10 02:50:15 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,23 +8,23 @@
 */
 int main(void)
 {
-			int i;
+	int i;
 
-					i = 48;
-							while  (i < 58)
-							{
-										putchar(i);
-												if (i !=  57)
-												{
-															putchar(44);
-																	putchar(32);
-																			i++;
-												}
-														else
-														{
-																	i++;
-														}
-							}
-									putchar('\n');
-											return (0);
+	i = 48;
+	while  (i < 58)
+{
+	putchar(i);
+	if (i !=  57)
+{
+	putchar(44);
+	putchar(32);
+	i++;
+}
+	else
+{
+	i++;
+}
+}
+	putchar('\n');
+	return (0);
 }
